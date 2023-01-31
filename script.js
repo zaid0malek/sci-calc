0ee3dbcfc040e7fe864e85c86c4d7aee5c7ed690
zaid0malek <zaid.m@simformsolutions.com>
2023-01-31 17:13:32 +0530

    new commit

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
+ip1 = document.getElementById("s1")
+ip2 = document.getElementById("s2")
 num1 = 0;
 op = "";
 num2 = "";
 arr = []
+
+ip = ip1
+ip2.value = 0;
+ip.focus();
+
+ip.onblur = function () {
+    setTimeout(function () {
+        ip.focus();
+        val = ip1.value;
+        ip1.value = '';
+        ip1.value = val;
+    }, 500);
+}
+
 function btnclick(x) {
-    y = document.getElementById("s1").value + x
-    document.getElementById("s1").value = y;
+    y = ip1.value + x
+    ip1.value = y;
 
-    num2 = num2 + x;
-    document.getElementById("s2").value = eval(num2);
+    // num2 = num2 + x;
+    // ip2.value = eval(num2);
     // calculate(num2);
-
+    calculate();
 
 }
 function opclick(x) {
-    y = document.getElementById("s1").value + x
-    document.getElementById("s1").value = y;
+    // y = ip1.value + x
+    ip1.value += x;
 
-    num2 = num2 + x;
-    // y = document.getElementById("s1").value + x
-    // document.getElementById("s1").value = y;
+    // num2 = num2 + x;
+    // y = ip1.value + x
+    // ip1.value = y;
     // arr = arr.concat(num2);
     // arr = arr.concat(x);
     // op = x;
@@ -53,3 +69,84 @@ function opclick(x) {
 
 
 // }
+function btnclear() {
+    ip1.value = "";
+    ip2.value = "";
+    num1 = 0;
+    op = "";
+    num2 = "";
+}
+function btnsquare() {
+
+
+    ip1.value = ip2.value + "^2";
+    ip2.value = ip2.value ** 2;
+}
+function btnback() {
+    num2 = ip1.value.slice(0, -1)
+    ip1.value = num2;
+    if (num2 == "") {
+        ip2.value = 0;
+
+    }
+    else {
+        ip2.value = eval(num2);
+    }
+}
+
+function calculate() {
+    val = ip1.value;
+
+    try {
+        if (val == "") {
+            ip2.value = 0;
+
+        }
+        else {
+            ip2.value = eval(val);
+        }
+    }
+    catch (err) {
+        ip2.value = "Error";
+    }
+}
+function btnequals() {
+    ip1.value = ip2.value;
+}
+function btnplusminus() {
+    ip2.value = ip2.value * -1;
+}
+function pi() {
+    ip1.value += 3.14159;
+    calculate();
+}
+function e() {
+    ip1.value += 2.71828;
+    calculate();
+}
+function btnfrac() {
+    ip1.value = ip2.value + "^-1"
+    ip2.value = ip2.value ** -1
+}
+function abs() {
+    ip2.value = Math.abs(ip2.value)
+}
+function btnsqrt() {
+
+    ip1.value = '\u221A' + ip1.value
+    ip2.value = Math.sqrt(ip2.value)
+}
+function fact() {
+    f = 1
+    n = ip1.value
+    for (i = 1; i <= n; i++) {
+        f *= i;
+
+    }
+
+    ip.value += '!'
+    ip2.value = f
+}
+function exp() {
+    ip1.value += "**"
+}

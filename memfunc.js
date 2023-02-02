b6091fa2907b8019c4b8486f279b9ef52999fc79
zaid0malek <122263800+zaid0malek@users.noreply.github.com>
2023-02-02 13:07:27 +0530

    Update memfunc.js

--- memfunc.js
+++ memfunc.js
@@ -1,7 +1,7 @@
 
 //Memory Store: stores output to memory/localstorage
 function ms() {
-    localStorage.setItem("Memory", ip2.value)
+    localStorage.setItem("Memory", op.value)
 
 }
 //Memory Recall: prints value which is available in memory
@@ -11,14 +11,14 @@ function mr() {
 }
 //Memory Add: takes the number on the display, adds it to the memory, and puts the result into memory
 function mplus() {
-    ip1.value = localStorage.getItem("Memory") + "+" + ip2.value
-    localStorage.setItem("Memory", eval(localStorage.getItem("Memory") + "+" + ip2.value))
+    ip1.value = localStorage.getItem("Memory") + "+" + op.value
+    localStorage.setItem("Memory", eval(localStorage.getItem("Memory") + "+" + op.value))
     calculate()
 }
 //Memory Subtract: takes the number on the display, subtracts it from memory, and puts the result into memory
 function mminus() {
-    ip1.value = localStorage.getItem("Memory") + "-" + ip2.value
-    localStorage.setItem("Memory", eval(localStorage.getItem("Memory") + "-" + ip2.value))
+    ip1.value = localStorage.getItem("Memory") + "-" + op.value
+    localStorage.setItem("Memory", eval(localStorage.getItem("Memory") + "-" + op.value))
     calculate()
 }
 //Memory Clear:sets the memory to 0
